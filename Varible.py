b6390ff4e9ac977c0322649a3dd5b628ba32b429
Ravi chandra <134200599+Ardnahcivar01@users.noreply.github.com>
2023-06-01 16:13:53 +0530

    Add files via upload

---
+++ Varible.py
@@ -0,0 +1,36 @@
+# Varibles in python programming language
+
+"""
+Python is dynamix type language which means we don't need to specify the varible type.
+
+varible --> Varible is nothing but refrence to the memory . In simple word varibles are the memory name which associated with some value in the memory
+
+"""
+
+varible1 = 10
+varible2 = 'string'
+varible3 = 10.0
+varible4 = 1992
+
+print(varible1)  
+print(varible2)
+print(varible3)
+print(varible4)
+
+# Checking alls type 
+
+print(type(varible1))
+print(type(varible2))
+print(type(varible3))
+print(type(varible4))
+
+# OUTPUT --> 10
+# string
+# 10.0
+# 1992
+# <class 'int'>
+# <class 'str'>
+# <class 'float'>
+# <class 'int'>
+
+
