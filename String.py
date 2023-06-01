e73076b1dddaa012ee44b52625b486720c0e2078
Ravi chandra <134200599+Ardnahcivar01@users.noreply.github.com>
2023-06-01 16:28:05 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,38 @@
+first = "hello users how are you !"
+second = "hey"
+long_string = """
+      WOW
+      O O
+      ---
+     """
+
+print(first)
+print(second)
+print(long_string)
+
+first = ' Ravi '
+second = ' chandra '
+
+print(f"{first} {second}")
+
+
+# String concatenation --> Combining two or more strings
+
+print ('Ravi'+'chandra')
+
+first,second = 'ravi' , 'chandra'
+str_concate = first + second
+
+print(str_concate)
+
+# outputs --> 
+# hello users how are you !
+# hey
+
+#       WOW
+#       O O
+#       ---
+     
+#  Ravi   chandra 
+# Ravichandra
+# ravichandra
