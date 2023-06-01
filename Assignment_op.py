60867a2febb56b9eb8928a3b64c1bc7c6c0cfa74
Ravi chandra <134200599+Ardnahcivar01@users.noreply.github.com>
2023-06-01 16:19:45 +0530

    Add files via upload

---
+++ Assignment_op.py
@@ -0,0 +1,22 @@
+num1 = 5
+num2 = 10
+
+# Assignment operator in python
+num1 += num2
+print(num1)
+
+num1 -= num2
+print(num1)
+
+num1 *= num2
+print(num1)
+
+
+num1 /= num2
+print(num1)
+
+# output -->
+# 15
+# 5
+# 50
+# 5.0
