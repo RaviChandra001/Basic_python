f8c6dc57da259bd62d8ee9967482808c38fb4772
Ravi chandra <134200599+Ardnahcivar01@users.noreply.github.com>
2023-06-02 01:32:44 +0530

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,37 @@
+# Matrix is a collection of rows and columns and we can say that it is a multi - dimensional array . Which means array insdie the array 
+
+# syntax --> matrix_name = [[ ]]
+
+matrix1 = [
+    [1,1,1],
+    [1,1,1],
+    [1,1,1]
+]
+
+print(matrix1[0][1])  # 5
+print(matrix1[0][0])  # 1
+print(matrix1[1][1])  # 1
+
+print(matrix1)
+
+matrix2=[
+    [1,1,1],
+    [1,1,1],
+    [1,1,1],
+]
+
+print(matrix2)
+
+# addition of matrix1 and 2 
+matrix3 = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+
+# addition using itreative approach
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[0])):
+        matrix3[i][j] = matrix1[i][j] + matrix2[i][j]
+    
+print("Resultant matrix is : ",matrix3)
