674847e1bce3940044afaf28f5cf5debb560212c
Ravi chandra <134200599+Ardnahcivar01@users.noreply.github.com>
2023-06-06 00:42:51 +0530

    Add files via upload

---
+++ Ternary_Operator.py
@@ -0,0 +1,10 @@
+# Ternary Operator in python 
+is_friend = True 
+can_msg = "message allowed" if is_friend else "not allowed"
+
+print(can_msg)
+
+is_friend = False
+can_msg2 = "Message allowed" if is_friend else "not Allowed"
+
+print(can_msg2)
